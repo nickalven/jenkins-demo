2360e36377d67bd6ebba0854b0d1b533c00e004a
Nikos <nickalven@hotmail.com>
2021-08-08 20:01:49 +0300

    changed createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,16 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/nickalven/ThemeParkRide.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
